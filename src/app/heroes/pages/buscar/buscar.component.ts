4065bbf9a3b2398fbd04af783912f3dd7beb7e67
Enrique <enrique@deltasoftinformatica.com>
2022-05-14 13:07:12 +0200

    Enrique 14-05 1

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -13,7 +13,7 @@ export class BuscarComponent implements OnInit {
   termino: string = "";
   heroes: Heroe[] = [];
 
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado!: Heroe | undefined;
 
   constructor( private heroesService: HeroesService) { }
 
@@ -22,12 +22,18 @@ export class BuscarComponent implements OnInit {
 
   buscando() {
 
-    this.heroesService.getSugerencias( this.termino )
-      .subscribe( heroes => this.heroes = heroes );
+    this.heroesService.getSugerencias( this.termino.trim() )
+      .subscribe( heroes => this.heroes = heroes);
 
   }
 
   opcionSeleccionada( event: MatAutocompleteSelectedEvent ) {
+
+    if (!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
     const heroe: Heroe = event.option.value;
     this.termino = heroe.superhero;
 
--- src/app/heroes/pipes/imagen-heroe.pipe.ts
+++ src/app/heroes/pipes/imagen-heroe.pipe.ts
@@ -7,8 +7,15 @@ import { Heroe } from '../interfaces/heroes.interface';
 export class ImagenHeroePipe implements PipeTransform {
 
   transform( heroe: Heroe): string {
-    /* return "assets/heroes/" + heroe.id + ".jpg"; */
-    return `assets/heroes/${ heroe.id }.jpg`
+    
+    if ( !heroe.id && !heroe.alt_img) {
+      return 'assets/no-image.png'
+    } else if ( heroe.alt_img ) {
+      return heroe.alt_img;
+    } else {
+      return `assets/heroes/${ heroe.id }.jpg`;
+    }
+    
   }
 
 }
